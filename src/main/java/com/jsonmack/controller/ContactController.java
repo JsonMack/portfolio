790eada612d3dc09dd5eabd563cc53d21c6db040
Jason <71078248+JsonMack@users.noreply.github.com>
2021-11-06 16:07:45 -0300

    Changed to prod.

--- src/main/java/com/jsonmack/controller/ContactController.java
+++ src/main/java/com/jsonmack/controller/ContactController.java
@@ -11,17 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.logging.Logger;
 
 /**
  * @author Jason MacKeigan
  */
 @Controller
-@RequestMapping("/contact")
 public class ContactController {
 
     private final ContactMessageRepository contactMessageRepository;
@@ -31,15 +32,17 @@ public class ContactController {
         this.contactMessageRepository = contactMessageRepository;
     }
 
-    @PostMapping
+    @PostMapping("/contact/create")
     public String post(@Valid @ModelAttribute ContactForm form, BindingResult result, RedirectAttributes attributes, HttpSession session) {
         if (result.hasErrors()) {
             attributes.addFlashAttribute("org.springframework.validation.BindingResult.contactForm", result);
             attributes.addFlashAttribute("contactForm", form);
-            return "contact";
+
+            return "redirect:/contact";
         }
         contactMessageRepository.save(new ContactMessage(form.getName(), form.getEmail(), form.getMessage()));
-        return "redirect:/contact_successful";
+
+        return "redirect:/contact";
     }
 
 }
--- src/main/java/com/jsonmack/controller/ContactSuccessfulController.java
+++ src/main/java/com/jsonmack/controller/ContactSuccessfulController.java
@@ -1,19 +1,31 @@
 package com.jsonmack.controller;
 
+import com.jsonmack.model.ContactForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * @author Jason MacKeigan
  */
-@Controller
-@RequestMapping("/contact_successful")
+@Controller()
 public class ContactSuccessfulController {
 
-    @GetMapping
-    public String index() {
-        return "contact_successful";
+    @GetMapping("/contact")
+    public ModelAndView view(Model model) {
+        if (!model.containsAttribute("contactForm")) {
+            model.addAttribute("contactForm", new ContactForm());
+        }
+        return new ModelAndView("contact/contact");
+    }
+
+    @GetMapping("/contact/success")
+    public String successView() {
+        return "contact/successful";
     }
 
 }
