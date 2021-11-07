cf2d40592ac7a6b83202051c1474057859fbadf1
Jason <71078248+JsonMack@users.noreply.github.com>
2021-11-07 15:48:43 -0400

    Changed back from 80 to 8080 and 443 to 8443

--- src/main/java/com/jsonmack/configuration/ProductionConfiguration.java
+++ src/main/java/com/jsonmack/configuration/ProductionConfiguration.java
@@ -42,9 +42,9 @@ public class ProductionConfiguration {
     public Connector httpsConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(8443);
         return connector;
     }
 
